86b805ce1b109f91e4fc50c8567fe4481be2fdfe
hlwbrian <hlwbrian@gmail.com>
2021-05-10 23:20:46 +0800

    update chat model

--- models/chatModel.js
+++ models/chatModel.js
@@ -1,52 +1,49 @@
 const mongoose = require('mongoose');
 
 const chatSchema = mongoose.Schema({
-    isGroup: {
-        type: Boolean,
-        required: [true, 'Is group indicator must be provided']
+    chatID: {
+        type: Number,
+        required: [true, 'A chatroom must have an ID'],
+        unqiue: [true, 'A chatroom must have an unique ID'],
+        default: 0
     },
     chatroomName: {
         type: String,
-        required: [true, 'A chatroom must have a chatroom name']
+        required: [true, 'A chatroom must have a chatroom name'],
+        maxlength: 32,
+        minlength: 1
     },
-    members: [String],
-    details: [{
+    members: [String], //save User.userID
+    conversations: [{
         sender: {
-            type: String
+            type: String //User.userID
         },
-        content: {
-            type: String
+        message: {
+            type: String,
+            minlength: 1,
+            maxlength: 255
         },
         timestamp: {
             type: Date,
             default: Date.now()
         }
-    }]
+    }],
+    chatCreate: {
+        type: Date,
+        default: Date.now()
+    }
 });
 
 const Chat = mongoose.model('Chat', chatSchema);
+
+//Auto increment chat ID
+chatSchema.pre('save', function(next){
+    let latestRecord = Chat.find().sort({chatCreate : -1});
+
+    if(latestRecord)
+        this.chatID = latestRecord.chatID + 1;
+
+    next();
+});
+
 module.exports = Chat;
-
-/**
-//Conversation Data Structure
-{
-	data: {
-		chatroomNo : Number,
-		conversations : [
-			{
-				chatid : String,
-				isGroup : Boolean,
-				members: [memberID],
-				details : [
-					{
-						sender : memberID
-						content : String,
-						timestamp : Datetime
-					}
-				]
-			}
-		]
-	}
-}
-
- */
