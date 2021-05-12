b5bdd11a37c7548424272f3316f59344f1945712
Brian Wong <hlwbrian@gmail.com>
2021-05-12 15:40:07 +0800

    update all related-chatroom when user change name

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('./../models/userModel');
+const Chat = require('./../models/chatModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.update = catchAsync(async (req, res, next) => {
@@ -7,16 +8,19 @@ exports.update = catchAsync(async (req, res, next) => {
     
     //find users
     const user = await User.findOne({userID : req.user.userID});
-    
+
     if(!user || !(await user.correctPassword(password, user.password))){
         res.status(404).json({
             status: 'failed',
             message: 'Incorrect Username or Password'
         })
     }else{
-        const updatedUser = await User.updateOne({userID: req.user.userID}, {username: username})
-        
-        if(updatedUser){
+        const updatedUser = await User.updateOne({userID: req.user.userID}, {username: username});
+        const updateChatroom = await Chat.updateMany({members: req.user.username + '#' + req.user.userID}, 
+                                                        {$set : { "members.$[element]" :  username + '#' + req.user.userID}},
+                                                        {arrayFilters : [{ "element" : req.user.username + '#' + req.user.userID}]}
+                                                    )
+        if(updatedUser && updateChatroom){
             res.status(201).json({
                 status: 'success',
                 message: 'information updated'
