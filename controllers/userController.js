9c6377dceb831e30e6dc15aea756d036368b5d8c
Brian Wong <hlwbrian@gmail.com>
2021-06-07 15:53:31 +0800

    update style for chatlist page

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,31 +8,24 @@ const AppError = require('../utils/appError');
 //Update personal information for user
 exports.updateUserName = catchAsync(async (req, res, next) => {
     let username = req.body.username;
-    let password = req.body.password;
-    
-    //find current user
-    const user = await User.findOne({userID : req.user.userID});
 
-    //check if the input password is corrected
-    if(!user || !(await user.correctPassword(password, user.password))){
-        return next(
-            new AppError('Incorrect password', 401)
-        );
-    }else{
-        //update the username in user collection
-        const updatedUser = await User.updateOne({userID: req.user.userID}, {username: username});
+    //update the username in user collection
+    const updatedUser = await User.updateOne({userID: req.user.userID}, {username: username});
 
-        //update all the related chat to show new username
-        const updateChatroom = await Chat.updateMany({members: req.user.username + '#' + req.user.userID}, 
+    //update all the related chat to show new username
+    const updateChatroom = await Chat.updateMany({members: req.user.username + '#' + req.user.userID}, 
                                                         {$set : { "members.$[element]" :  username + '#' + req.user.userID}},
                                                         {arrayFilters : [{ "element" : req.user.username + '#' + req.user.userID}]}
                                                     );
-        if(updatedUser && updateChatroom){
-            res.status(201).json({
-                status: 'success',
-                message: 'user information updated'
-            });
-        }
+    if(updatedUser && updateChatroom){
+        res.status(201).json({
+            status: 'success',
+            message: 'user information updated'
+        });
+    }else{
+        return next(
+            new AppError('Update failed', 401)
+        );
     }
 });
 
